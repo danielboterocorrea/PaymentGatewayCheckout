a49d44d074abf83ed8690fa30474dd6782bd84d8
Daniel Botero Correa <daniel_boteroc4@hotmail.com>
2020-03-24 10:46:26 +0100

    adding certificates

--- build.cmd
+++ build.cmd
@@ -3,3 +3,6 @@ dotnet build
 docker build -t acquiringbanksimulator -f AcquiringBank.Simulator/Dockerfile .
 docker build -t paymentgatewayapi -f PaymentGateway.Api/Dockerfile .
 docker build -t identityserver -f PaymentGateway.IdentityServer/Dockerfile .
+::Create certificates
+dotnet dev-certs https -ep ${HOME}/.aspnet/https/aspnetapp.pfx -p admin
+dotnet dev-certs https --trust
